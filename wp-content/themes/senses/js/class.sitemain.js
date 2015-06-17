2ba945eba17defc5e80266160dab6bc39f04bb04
Binh <binhdarkcu@gmail.com>
2015-06-17 00:44:42 -0700

    update template

--- wp-content/themes/senses/js/class.sitemain.js
+++ wp-content/themes/senses/js/class.sitemain.js
@@ -1,35 +1,41 @@
-var SiteMain = (function() {
+var siteMain = (function () {
     //INIT
-	function init(){
-		initEvent();
-        initCSS();
+    function init() {
+        createDatepicker('#birthday');
+        getCountries();
 	}
-	
-	function initEvent(){
-	}
-
-    function initCSS(){
-    	$('#header').height($(window).height());
-    	$(window).resize(function(){
-    		$('#header').height($(window).height());
-    	});
+    function scrolltoDiv(idSelector) {
+		var targetOffset = $(idSelector).offset().top;
+        $('html,body').animate({scrollTop: targetOffset}, 1000);
     }
-
-	function openPopup(idDiv){
-		jQuery('.popup').css('display','none');
-		jQuery(idDiv).css('display','block');
-	}
-	
-	function closePopup(){
-		jQuery('.popup').css('display','none');
-	}
-	
+    function createDatepicker(idSelector) {
+        $(idSelector).datepicker({
+            inline: true,
+            changeMonth: true,//this option for allowing user to select month
+            changeYear: true, //this option for allowing user to select from year range,
+            yearRange: '1900:c',
+            beforeShow:function(textbox, instance){
+                $('.birthdayCol').append($('#ui-datepicker-div'));
+                $('#ui-datepicker-div').hide();
+            },
+            onChangeMonthYear: function (y, m, i) {
+                var d = i.selectedDay;
+                $(this).datepicker('setDate', new Date(y, m - 1, d));
+            }
+        });
+        $('.ui-datepicker-trigger').click(function () {
+            $(idSelector).datepicker().focus();
+        });
+    }
+    function getCountries() {
+        populateCountries("countries");
+    }
 	return {
-		init:init
+		init:init,
+		scrolltoDiv:scrolltoDiv
 	};
-	
-})();		
-
-$(document).ready( function() {
-	SiteMain.init();
+})();
+$(document).ready(function () {
+    siteMain.init();
 });
+
